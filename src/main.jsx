23b4f6587697a75c8f24f632961e1fe0a0120cb2
derpy4me <bittle1450@gmail.com>
2022-01-13 14:24:48 -0700

    added functionality for routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { render } from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './main.css';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
 
 let rootElement = document.getElementById('app');
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Route path="/" element={<App />} />
+    <Route path="expenses" element={<Expenses />} />
+    <Route path="invoices" element={<Invoices />} />
   </BrowserRouter>,
   rootElement
 );
